5d3291ae1bdd48ac5d7d6f4012593d504dadf766
Firdows <ahamad.jedu@gmail.com>
2017-08-01 09:33:47 +0000

    backup

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,17 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        // 'dist/metisMenu/metisMenu.min.css',
+        // 'dist/css/sb-admin-2.css',
+        // 'dist/morrisjs/morris.css',
+        // 'dist/font-awesome/css/font-awesome.min.css',
         'css/site.css',
     ];
     public $js = [
+        // 'dist/metisMenu/metisMenu.min.js',
+        // 'dist/raphael/raphael.min.js',
+        // 'dist/morrisjs/morris.min.js',
+        // 'dist/js/sb-admin-2.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- backend/config/main.php
+++ backend/config/main.php
@@ -12,12 +12,28 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
+    'homeUrl' => '/office',
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+            'layout' => 'left-menu',
+        ],
+        'user' => [
+           'class' => 'dektrium\user\Module',
+            'enableUnconfirmedLogin' => true,
+            'confirmWithin' => 21600,
+            'cost' => 12,
+            'admins' => ['admin']
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
+            'baseUrl' => '/office',
         ],
+        
         'user' => [
-            'identityClass' => 'common\models\User',
+            'identityClass' => 'dektrium\user\models\User',
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
@@ -37,14 +53,46 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
+//       'view' => [
+// 			'theme' => [
+// 				'pathMap' => [
+// 					'@app/views/layouts' => '@vendor/p2made/yii2-sb-admin-theme/views/sb-admin-2/layouts',
+// 				],
+// 			],
+// 		],
+    'assetManager' => [
+        'bundles' => [
+            'dmstr\web\AdminLteAsset' => [
+                'skin' => 'skin-black-light',
+            ],
+        ],
     ],
+    ],
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            'user/*',
+            'debug/*',
+            'gii/*',
+            'some-controller/some-action',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
+    ],
+    
+    
     'params' => $params,
 ];
